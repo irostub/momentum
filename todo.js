f50576e604b4c04ebd5296d1538170a725201ab7
irostub <irostub@gmail.com>
2021-02-17 18:38:53 +0900

    Implement todo list

--- todo.js
+++ todo.js
@@ -1,14 +1,38 @@
 const TODO_CONTAINER = document.querySelector(".js-todo");
 const input = TODO_CONTAINER.querySelector("input");
 const todoForm = TODO_CONTAINER.querySelector("form");
+const button = TODO_CONTAINER.querySelector("button");
 
 let todoList = [];
+let toggleSlide = false;
 
 function handleSubmit(e) {
   e.preventDefault();
   addTodo(input.value);
   saveTodos();
   input.value = "";
+}
+
+function handleClick() {
+  const clock = document.querySelector(".js-clock");
+  const name = document.querySelector(".js-name");
+  if (toggleSlide) {
+    TODO_CONTAINER.classList.add("slidedown");
+    TODO_CONTAINER.classList.remove("slideup");
+    clock.classList.add("fadein");
+    clock.classList.remove("fadeout");
+    name.classList.add("fadein");
+    name.classList.remove("fadeout");
+    toggleSlide = !toggleSlide;
+  } else {
+    TODO_CONTAINER.classList.add("slideup");
+    TODO_CONTAINER.classList.remove("slidedown");
+    clock.classList.add("fadeout");
+    clock.classList.remove("fadein");
+    name.classList.add("fadeout");
+    name.classList.remove("fadein");
+    toggleSlide = !toggleSlide;
+  }
 }
 
 function addTodo(text) {
@@ -18,7 +42,7 @@ function addTodo(text) {
   const button = document.createElement("button");
   li.id = todoList.length;
   span.innerText = text;
-  button.innerText = "delete";
+  button.innerHTML = `<img src="./x.svg"/>`;
 
   button.addEventListener("click", removeTodo);
 
@@ -31,8 +55,9 @@ function addTodo(text) {
 }
 
 function removeTodo(e) {
-  todoList = todoList.filter((todo) => todo.id !== parseInt(e.target.parentNode.id));
-  e.target.parentNode.remove();
+  console.log(e.target.parentNode.parentNode);
+  todoList = todoList.filter((todo) => todo.id !== parseInt(e.target.parentNode.parentNode.id));
+  e.target.parentNode.parentNode.remove();
   saveTodos();
 }
 
@@ -52,6 +77,7 @@ function loadTodos() {
 function init() {
   todoForm.addEventListener("submit", handleSubmit);
   loadTodos();
+  button.addEventListener("click", handleClick);
 }
 
 init();
